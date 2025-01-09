015261af01645f5dcd549570faf3b1b977d401a7
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-01-09 15:49:35 +0100

    Init

---
+++ src/main/java/de/thaum/lifedatabase/LifeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package de.thaum.lifedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeDatabaseApplication.class, args);
+    }
+
+}
